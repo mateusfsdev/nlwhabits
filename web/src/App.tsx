6696a1f6ad79360e78eb195f24d0f52729bf8093
mateus sant'ana <mateusfdev@outlook.com>
2024-02-13 03:03:36 -0300

    create mobile 📳📱📲

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,17 @@
-import { Habit } from "./components/Habit";
+import { Habit } from "./components/habit";
 
 export default function App() {
   return (
-    <div>
-      <h1>hello</h1>
-      <Habit completed={3}/>
-      <Habit completed={5}/>
-      <Habit completed={6}/>
-      <Habit completed={8}/>
+    <div className="flex h-[100vh] flex-col bg-stone-800 justify-center items-center">
+ 
+      <h1 className="font-bold text-3xl text-purple-500"
+        >hello
+      </h1>
+      <Habit completed={33} />
+      <Habit completed={24} />
+      <Habit completed={15} />
+      <Habit completed={34} />
+      <Habit completed={18} />
 
     </div>
   )
